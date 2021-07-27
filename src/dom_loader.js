efc1e480aea1d304d1f1c6528adbdf97834699dd
Spencer Knight <spencer.knightusn@gmail.com>
2021-07-27 09:02:49 -0700

    added nav_bar elements

--- src/dom_loader.js
+++ src/dom_loader.js
@@ -1,5 +1,6 @@
 import "./index.css";
-import { HEADER, META_DATA } from "./ui/ui.js";
+import { HEADER, META_DATA, NAV_BAR } from "./ui/ui.js";
 
 META_DATA();
 HEADER();
+NAV_BAR();
--- src/ui/ui.js
+++ src/ui/ui.js
@@ -37,4 +37,46 @@ const HEADER = () => {
   HEADER_CONTAINER.append(NAV_BAR_TEXT);
 };
 
-export { HEADER, META_DATA };
+const NAV_BAR = () => {
+  const NAV_CONTAINER = document.createElement("nav");
+  const NAV_BAR_LIST = document.createElement("ol");
+  const DUE_TODAY = document.createElement("li");
+  const DUE_THIS_WEEK = document.createElement("li");
+  const AGENDAS_TITLE = document.createElement("h2");
+  const AGENDA_CONTAINER = document.createElement("ol");
+  const ADD_AGENDA = document.createElement("button");
+
+  NAV_CONTAINER.id = "nav_container";
+  NAV_BAR_LIST.id = "nav_bar_list";
+  DUE_TODAY.id = "due_today";
+  DUE_THIS_WEEK.id = "due_this_week";
+  AGENDAS_TITLE.id = "agendas_title";
+  AGENDA_CONTAINER.id = "agenda_container";
+  ADD_AGENDA.id = "add_agenda";
+
+  const CLASSLIST = [
+    DUE_TODAY,
+    DUE_THIS_WEEK,
+    AGENDAS_TITLE,
+    AGENDA_CONTAINER,
+    ADD_AGENDA,
+  ];
+  for (let i = 0; i < CLASSLIST.length; i++) {
+    CLASSLIST[i].classList = "nav_item";
+  }
+
+  DUE_TODAY.innerText = "Today";
+  DUE_THIS_WEEK.innerText = "Week";
+  AGENDAS_TITLE.innerText = "AGENDAS";
+  ADD_AGENDA.innerText = "add agenda";
+
+  document.body.append(NAV_CONTAINER);
+  NAV_CONTAINER.append(NAV_BAR_LIST);
+  NAV_BAR_LIST.append(DUE_TODAY);
+  NAV_BAR_LIST.append(DUE_THIS_WEEK);
+  NAV_BAR_LIST.append(AGENDAS_TITLE);
+  NAV_BAR_LIST.append(AGENDA_CONTAINER);
+  NAV_BAR_LIST.append(ADD_AGENDA);
+};
+
+export { HEADER, META_DATA, NAV_BAR };
