e143932398d13e2d5db79bedf8d7865c585fae74
Spencer Knight <spencer.knightusn@gmail.com>
2021-07-27 13:20:37 -0700

    removed desktop media queries / added button for adding task groups

--- src/dom_loader.js
+++ src/dom_loader.js
@@ -1,6 +1,7 @@
 import "./index.css";
-import { HEADER, META_DATA, NAV_BAR } from "./ui/ui.js";
+import { HEADER, META_DATA, NAV_BAR, TOGGLE_BUTTON } from "./ui/ui.js";
 
 META_DATA();
 HEADER();
 NAV_BAR();
+TOGGLE_BUTTON();
--- src/ui/ui.js
+++ src/ui/ui.js
@@ -15,16 +15,16 @@ const META_DATA = () => {
   document.head.append(FONT_AWESOME);
 };
 
-// const TOGGLE_BUTTON = () => {
-//   const NAV_BAR_TOGGLE = document.createElement("button");
-//   const NAV_BAR_ICON = document.createElement("i");
+const TOGGLE_BUTTON = () => {
+  const NAV_BAR_TOGGLE = document.createElement("button");
+  const NAV_BAR_ICON = document.createElement("i");
 
-//   NAV_BAR_TOGGLE.id = "nav_bar_toggle_button";
-//   NAV_BAR_ICON.classList = "fas fa-align-justify";
+  NAV_BAR_TOGGLE.id = "nav_bar_toggle_button";
+  NAV_BAR_ICON.classList = "fas fa-align-justify";
 
-//   document.body.append(NAV_BAR_TOGGLE);
-//   NAV_BAR_TOGGLE.append(NAV_BAR_ICON);
-// };
+  document.body.append(NAV_BAR_TOGGLE);
+  NAV_BAR_TOGGLE.append(NAV_BAR_ICON);
+};
 
 const HEADER = () => {
   const HEADER_CONTAINER = document.createElement("header");
@@ -33,7 +33,6 @@ const HEADER = () => {
   NAV_BAR_TEXT.innerText = "Task Master";
 
   document.body.append(HEADER_CONTAINER);
-  // TOGGLE_BUTTON();
   HEADER_CONTAINER.append(NAV_BAR_TEXT);
 };
 
@@ -42,41 +41,42 @@ const NAV_BAR = () => {
   const NAV_BAR_LIST = document.createElement("ol");
   const DUE_TODAY = document.createElement("li");
   const DUE_THIS_WEEK = document.createElement("li");
-  const AGENDAS_TITLE = document.createElement("h2");
-  const AGENDA_CONTAINER = document.createElement("ol");
-  const ADD_AGENDA = document.createElement("button");
+  const GROUP_TITLE = document.createElement("h2");
+  const GROUP_CONTAINER = document.createElement("ol");
+  const GROUP_GROUP = document.createElement("button");
+  const PLUS_ICON = document.createElement("i");
 
   NAV_CONTAINER.id = "nav_container";
   NAV_BAR_LIST.id = "nav_bar_list";
   DUE_TODAY.id = "due_today";
   DUE_THIS_WEEK.id = "due_this_week";
-  AGENDAS_TITLE.id = "agendas_title";
-  AGENDA_CONTAINER.id = "agenda_container";
-  ADD_AGENDA.id = "add_agenda";
+  GROUP_TITLE.id = "group_title";
+  GROUP_CONTAINER.id = "task_group_container";
+  GROUP_GROUP.id = "add_group";
+  PLUS_ICON.id = "add_group_plus_sign";
 
-  const CLASSLIST = [
+  const VISIBLE_NAV_ITEMS = [
     DUE_TODAY,
     DUE_THIS_WEEK,
-    AGENDAS_TITLE,
-    AGENDA_CONTAINER,
-    ADD_AGENDA,
-  ];
-  for (let i = 0; i < CLASSLIST.length; i++) {
-    CLASSLIST[i].classList = "nav_item";
-  }
+    GROUP_TITLE,
+    GROUP_CONTAINER,
+    GROUP_GROUP,
+  ].map((element) => (element.classList = "nav_item"));
+  PLUS_ICON.classList = "fas fa-plus-circle";
 
   DUE_TODAY.innerText = "Today";
   DUE_THIS_WEEK.innerText = "Week";
-  AGENDAS_TITLE.innerText = "AGENDAS";
-  ADD_AGENDA.innerText = "add agenda";
+  GROUP_TITLE.innerText = "Groups";
+  GROUP_GROUP.innerText = "group";
 
   document.body.append(NAV_CONTAINER);
   NAV_CONTAINER.append(NAV_BAR_LIST);
   NAV_BAR_LIST.append(DUE_TODAY);
   NAV_BAR_LIST.append(DUE_THIS_WEEK);
-  NAV_BAR_LIST.append(AGENDAS_TITLE);
-  NAV_BAR_LIST.append(AGENDA_CONTAINER);
-  NAV_BAR_LIST.append(ADD_AGENDA);
+  NAV_BAR_LIST.append(GROUP_TITLE);
+  NAV_BAR_LIST.append(GROUP_CONTAINER);
+  NAV_BAR_LIST.append(GROUP_GROUP);
+  GROUP_GROUP.prepend(PLUS_ICON);
 };
 
-export { HEADER, META_DATA, NAV_BAR };
+export { HEADER, META_DATA, NAV_BAR, TOGGLE_BUTTON };
