2037d0b91a61cfc1ebeb6b31efc13119c4721568
Spencer Knight <spencer.knightusn@gmail.com>
2021-07-27 22:01:30 -0700

    created Group class

--- app.bundle.js
+++ app.bundle.js
@@ -3,7 +3,18 @@ var __webpack_exports__ = {};
 /*!********************!*\
   !*** ./src/app.js ***!
   \********************/
+let groups = [];
 
+const Group = (label) => {
+  const LABEL = label;
+  let notes = [];
+
+  const GET_NOTES = () => notes;
+  const GET_LABEL = () => LABEL;
+
+  return { GET_LABEL, GET_NOTES };
+};
 
 /******/ })()
 ;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvLy4vc3JjL2FwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBO0FBQ0E7QUFDQTs7QUFFQTtBQUNBOztBQUVBLFVBQVU7QUFDViIsImZpbGUiOiJhcHAuYnVuZGxlLmpzIiwic291cmNlc0NvbnRlbnQiOlsibGV0IGdyb3VwcyA9IFtdO1xuXG5jb25zdCBHcm91cCA9IChsYWJlbCkgPT4ge1xuICBjb25zdCBMQUJFTCA9IGxhYmVsO1xuICBsZXQgbm90ZXMgPSBbXTtcblxuICBjb25zdCBHRVRfTk9URVMgPSAoKSA9PiBub3RlcztcbiAgY29uc3QgR0VUX0xBQkVMID0gKCkgPT4gTEFCRUw7XG5cbiAgcmV0dXJuIHsgR0VUX0xBQkVMLCBHRVRfTk9URVMgfTtcbn07XG4iXSwic291cmNlUm9vdCI6IiJ9
